d38933fedb065e916e6e897ef1381dcf5e262833
ressamelinda <ressamelinda02@gmail.com>
2024-05-05 10:21:51 +0700

    ..

--- resources/views/admin/layouts/_message.blade.php
+++ resources/views/admin/layouts/_message.blade.php
@@ -1,18 +1,18 @@
 @if(!empty(session('success')))
- <div class="alert alert-success " role="alert">
+ <div class="alert alert-success" role="alert">
     {{ session('success') }}
  </div>
 @endif
 
 @if(!empty(session('error')))
- <div class="alert alert-danger " role="alert">
+ <div class="alert alert-danger" role="alert">
     {{ session('error') }}
  </div>
 @endif
 
 @if(!empty(session('payment-error')))
- <div class="alert alert-error " role="alert">
-    {{ session('warning') }}
+ <div class="alert alert-danger" role="alert">
+    {{ session('payment-error') }}
  </div>
 @endif
 
@@ -23,25 +23,25 @@
 @endif
 
 @if(!empty(session('info')))
- <div class="alert alert-info " role="alert">
+ <div class="alert alert-info" role="alert">
     {{ session('info') }}
  </div>
 @endif
 
 @if(!empty(session('secondary')))
- <div class="alert alert-seondary " role="alert">
+ <div class="alert alert-secondary" role="alert">
     {{ session('secondary') }}
  </div>
 @endif
 
 @if(!empty(session('primary')))
- <div class="alert alert-primary " role="alert">
+ <div class="alert alert-primary" role="alert">
     {{ session('primary') }}
  </div>
 @endif
 
 @if(!empty(session('light')))
- <div class="alert alert-light " role="alert">
+ <div class="alert alert-light" role="alert">
     {{ session('light') }}
  </div>
 @endif
