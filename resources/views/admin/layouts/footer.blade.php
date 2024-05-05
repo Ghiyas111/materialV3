47ef550b0613b12debacc430b8d54b5936d868bd
Ghiyas111 <143326834+Ghiyas111@users.noreply.github.com>
2024-05-05 12:25:00 +0700

    1

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -2,10 +2,10 @@
 </aside>
 
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Material</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 0.0.0
     </div>
   </footer>
 </div>
